0414462e5eb7851f5c4b3fb123497cf0f748a28e
LuchoPonce <luis.ponce@utec.edu.pe>
2019-08-22 22:27:19 -0700

    Final commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
+
+	int casos,muro,altura,temp,actual,up=0,down=0;
+
+	cin>>casos;
+
+	for(int i=0;i<casos;i++){
+		cin>>muro;
+		for(int j=0;j<muro;j++){
+			if(j==0){
+				cin>>temp;	
+			}
+			else{
+				cin>>actual;
+				if(actual>temp){
+					up++;
+				}
+				else if(actual<temp){
+					down++;
+				}
+				temp = actual;
+			}
+			
+		}
+		cout<<"Case "<<i+1<<": "<<up<<" "<<down<<endl;
+		up = 0;
+		down = 0;
+	}
+
 	return EXIT_SUCCESS;
 }
 
